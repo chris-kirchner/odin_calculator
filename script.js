b1754f290a6329eb70060c360c5b3604cfd23711
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-06-15 15:04:38 -0700

    Add initial code for displaying numbers on click

--- script.js
+++ script.js
@@ -35,4 +35,30 @@ function operate(operator, num1, num2) {
     return "Error";
   };
 };
+let display1 = document.getElementById("display1");
+let display2 = document.getElementById("display2");
+let allButtons = document.getElementsByTagName("button");
+for (let i = 0; i < allButtons.length; i++) {
+  allButtons[i].addEventListener("click", buttonClick);
+}
+let digitRegex = /\d/g;
 
+let numsEntered = 0;
+function buttonClick(e) {
+  if (e.target.innerText === "C") {
+    display2.innerText = 0;
+    numsEntered = 0;
+  }
+  if (numsEntered === 0 && parseInt(e.target.innerText) === 0) {
+  }
+  else if (numsEntered === 0 && e.target.innerText >= 0 && e.target.innerText <= 9) {
+    display2.innerText = e.target.innerText;
+    numsEntered = 1;
+  }
+  else {
+    if (e.target.innerText >= 0 && e.target.innerText <= 9) {
+      display2.innerText += e.target.innerText;
+    }
+  }
+  
+}
