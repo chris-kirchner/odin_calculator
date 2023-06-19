c06f43963ea206a6f012c875864858e841bc6155
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-06-18 17:52:06 -0700

    Add fontScale() to dynamically scale text on display2

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function operate(operator, num1, num2) {
     return "Error";
   };
 };
+let displayContainer = document.getElementById("display-container");
 let display1 = document.getElementById("display1");
 let display2 = document.getElementById("display2");
 let allButtons = document.getElementsByTagName("button");
@@ -44,6 +45,22 @@ let num2 = 0;
 let numEntered = 0;
 let decimalEntered = 0;
 let answer = 0;
+let regEx = /\d+/g;
+display2.style.fontSize = "70px";
+
+function fontScale() {
+  if (display2.clientWidth <= 358) {
+    display2.style.fontSize = "70px";
+  }
+  if (display2.clientWidth > 358) {
+    // display2FontSize = regEx.exec(display2.style.fontSize)[0];
+    let display2FontSize = 70;
+    while (display2.clientWidth > 358) {
+      display2FontSize -= 1;
+      display2.style.fontSize = display2FontSize + "px";
+    }
+  }
+};
 
 function buttonClick(e) {
   if (e.target.innerText === "C") {
@@ -53,27 +70,34 @@ function buttonClick(e) {
     decimalEntered = 0;
     answer = 0;
     operator = "";
+    display2.style.fontSize = "70px";
   }
 
-  if (e.target.innerText === "+") {
+  if (e.target.innerText === "+" || e.target.innerText === "-" || e.target.innerText === "*" || e.target.innerText === "/") {
     num1 = parseFloat(display2.innerText);
-    operator = "+";
-    display1.innerText = display2.innerText + " " + operator;
+    operator = e.target.innerText;
+    display1.innerText = `${display2.innerText} ${operator}`;
     decimalEntered = 0;
     numEntered = 0;
-    // display2.innerText = "";
   }
 
   if (e.target.innerText === "=" && operator !== "" && answer === 0) {
     num2 = parseFloat(display2.innerText);
-    display1.innerText += " " + display2.innerText + " =";
+    display1.innerText += ` ${display2.innerText} =`;
     display2.innerText = operate(operator, num1, num2);
     answer = 1;
+    fontScale();
   }
   else if (e.target.innerText === "=" && operator !== "" && answer === 1) {
     num1 = parseFloat(display2.innerText);
-    display1.innerText = num1 + " " + operator + " " + num2 + " =";
+    display1.innerText = `${num1} ${operator} ${num2} =`;
     display2.innerText = operate(operator, num1, num2);
+    fontScale();
+  }
+  else if (e.target.innerText === "=" && operator === "" && answer === 0) {
+    num1 = parseFloat(display2.innerText);
+    display1.innerText = num1 + " =";
+    fontScale();
   }
 
   if (e.target.innerText === "." && decimalEntered === 0 && operator === "") {
@@ -113,3 +137,20 @@ function buttonClick(e) {
     }
   }
 }
+
+
+
+// display2.style.fontSize = "70px";
+// let regExNum = parseInt(regEx.exec(display2.style.fontSize)[0]);
+// console.log(regExNum);
+// console.log(display2.clientWidth);
+// console.log(displayContainer.clientWidth);
+
+// if (display2.clientWidth > 360) {
+//   display2.style.fontSize = "70px";
+//   let newFontSize = parseInt(regEx.exec(display2.style.fontSize)[0]);
+//   while (display2.clientWidth > 360) {
+//     newFontSize -= 1;
+//     display2.style.fontSize = newFontSize + "px";
+//   }
+// }
